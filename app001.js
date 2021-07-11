75e01c14d554e3f8abd9718d32f16ac41283cd0c
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-07-11 16:31:14 +0100

    add some files

---
+++ app001.js
@@ -0,0 +1,12 @@
+console.log({a:1,b:2});
+console.table({a:1,b:2});//show in table 
+
+console.time('hello');//hello is Id  begin of section
+console.log({a:1,b:2});
+console.log({a:1,b:2});
+console.log({a:1,b:2});
+console.log({a:1,b:2});
+console.log({a:1,b:2});
+console.log({a:1,b:2});
+console.timeEnd('hello');//end of section 
+                    //and in the end show time that took
