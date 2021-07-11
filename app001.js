4be6f55cff3d3633a191b155e9464d723899d202
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-07-12 00:20:09 +0100

    add some explaintation01

--- app001.js
+++ app001.js
@@ -84,3 +84,93 @@ let day =now.getDate();//day
 //Immidiatley Invokable Function Expressions -IIFEs
 // run function immediately
 (function(namefun){console.log("hello "+namefun);})('brad');//(for function)(parameters);
+
+/* Window */
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// Alert
+//alert('Hello World');
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if(confirm('Are you sure')){
+//   console.log('YES');
+// } else {
+//   console.log('NO');
+// }
+
+let val;
+
+// Outter height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points
+val = window.scrollY;
+val = window.scrollX;
+
+// Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// Redirect
+//window.location.href = 'http://google.com';
+//Reload
+//window.location.reload();
+
+// History Object
+
+// window.history.go(-2);
+// val = window.history.length;
+
+// Navigator Object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+
+
+console.log(val);
+//****************************************************** */
+// Global Scope
+var a = 1;
+let b = 2;
+const c = 3;
+
+// function test() {
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+//   console.log('Function Scope: ', a, b, c);
+// }
+
+// test();
+
+// if(true) {
+//   // Block Scope
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+//   console.log('If Scope: ', a, b, c);
+// }
+
+// for(var a = 0; a < 10; a++) {
+//   console.log(`Loop: ${a}`);
+// }
+
+console.log('Global Scope: ', a, b, c);
+//***************************************************************** */
